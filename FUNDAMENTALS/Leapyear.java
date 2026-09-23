6ddfd1652299779d8db3203b02ee830f694abe79
Harpalop <harpalsolanki999@gmail.com>
2026-09-23 14:11:20 +0530

    Fundamentals of java

---
+++ FUNDAMENTALS/Leapyear.java
@@ -0,0 +1,23 @@
+package FUNDAMENTALS;
+
+import java.util.Scanner;
+
+public class Leapyear{
+
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("enter a year");
+            int year = sc.nextInt();
+
+            if((year%400==0) || (year%4==0 && year%100!=0 )){
+                System.out.println("its a leap year!");
+            } 
+            else{
+                System.out.println("its not a leap year.");
+            }
+
+            sc.close();
+        }
+}
